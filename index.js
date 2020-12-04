55e69cfedb0a0f42a10f94e29741ae18b0435d32
Angus Kellett <anguskellett@hotmail.com>
2020-12-04 04:37:37 +0000

    prompts user with flag, checks answer

--- index.js
+++ index.js
@@ -15,15 +15,32 @@ const flags = { afghanistan : '🇦🇫',
   armenia: '🇦🇲', 
   australia: '🇦🇺', 
   austria: '🇦🇹', 
-  azerbaijan: '🇦🇿'};
+  azerbaijan: '🇦🇿'
+};
 
+let keys = Object.keys(flags);
 
-rl.question("Welcome to Guess The Flag. In this game you will be shown the flag of a country and you must correctly identify it. Type 'play' to continue.", (answer) => {
-  if(answer === "play") {
+rl.question("Welcome to Guess The Flag. In this game you will be shown the flag of a country and you must correctly identify it. Type 'play' to continue.\n", (answer1) => {
+  if(answer1 === "play") {
     const poseQuestion = function() {
-      Math.floor(Math.random() * 9) + 1;
-      
+      let index = Math.floor(Math.random() * 9) + 1;
+      let randomCountryName = keys[index];
+      let randomFlag = flags[`${randomCountryName}`];
+      rl.question(`What country has this flag?: ${randomFlag}\n`, (answer2) => {
+        let guess = answer2.toLowerCase().trim().replace(/\s+/g, "");
+        if(guess === keys[index]) {
+          console.log("Correct!");
+          poseQuestion(); 
+        } else {
+          console.log("Wrong! Game over.")
+          rl.close();
+        }
+      });
     }
+    poseQuestion();
+  } else if (answer1 !== "play") {
+    console.log(keys);
+    console.log("Fuck right off then. And don't bother coming back.")
+    rl.close();
   }
-}
-)
+});
