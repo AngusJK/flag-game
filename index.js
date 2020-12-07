852adc2da58549149bb48df2e5d2123a8ebd5a53
Angus Kellett <anguskellett@hotmail.com>
2020-12-07 21:55:33 +0000

    creates answerNumber function to assign country randomly to multiple choice answers

--- index.js
+++ index.js
@@ -13,23 +13,38 @@ rl.question("Welcome to Guess The Flag. In this game you will be shown ten(10) c
     let numOfGuesses = 0;
     let numOfCorrectGuesses = 0;
     const poseQuestion = function() {
-      let index = Math.floor(Math.random() * 8);
+      let index = Math.floor(Math.random() * 193);
       let randomCountryName = keys[index];
       let randomFlag = flags[`${randomCountryName}`]["flag"];
-      let option1 = keys[Math.floor(Math.random() * 193)];
-      let option2 = keys[Math.floor(Math.random() * 193)];
-      let option3 = keys[Math.floor(Math.random() * 193)];
-      let option4 = keys[Math.floor(Math.random() * 193)];      
+      let option1 = flags[keys[Math.floor(Math.random() * 193)]]["name"];
+      let option2 = flags[keys[Math.floor(Math.random() * 193)]]["name"];
+      let option3 = flags[keys[Math.floor(Math.random() * 193)]]["name"];
+      let option4 = flags[keys[Math.floor(Math.random() * 193)]]["name"];
+      let num = Math.floor(Math.random() * 3 + 1);
+      let correctNumber = num;
+      let answerNumber = function() {
+        if (num === 1) {
+          option1 = flags[`${randomCountryName}`]["name"];
+        } else if (num === 2) {
+          option2 = flags[`${randomCountryName}`]["name"];
+        } else if (num === 3) {
+          option3 = flags[`${randomCountryName}`]["name"];
+        } else if (num === 4) {
+          option4 = flags[`${randomCountryName}`]["name"];
+        }
+      }
+      answerNumber(num);
       let multipleChoice = `  1. ${option1}\n  2. ${option2}\n  3. ${option3}\n  4. ${option4}\n`;
       rl.question(`What country has this flag?: ${randomFlag}\n${multipleChoice}`, (answer2) => {
-        let guess = answer2.toLowerCase().trim().replace(/\s+/g, "");
-        if (guess === keys[index]) {
+        let guess = Number(answer2);
+        //let guess = answer2.toLowerCase().trim().replace(/\s+/g, "");
+        if (guess === correctNumber) {
           numOfCorrectGuesses += 1;
           numOfGuesses += 1;
           console.log(`✅ Correct! ${numOfCorrectGuesses} out of ${numOfGuesses}.`);
         } else {
           numOfGuesses += 1;
-          console.log(`❌ Wrong! The answer is ${keys[index]}. ${numOfCorrectGuesses} out of ${numOfGuesses}.`);
+          console.log(`❌ Wrong! The answer is ${flags[randomCountryName]["name"]}. ${numOfCorrectGuesses} out of ${numOfGuesses}.`);
         }
         if (numOfGuesses === 10) {
           console.log(`Game over. Final score: ${numOfCorrectGuesses} correct out of ${numOfGuesses}.`);
